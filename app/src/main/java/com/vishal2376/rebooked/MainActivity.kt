16d6647a5bfb44d77be0423c440f89fa83925c04
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-04 12:35:50 +0530

    update: bottom nav setup done

--- app/src/main/java/com/vishal2376/rebooked/MainActivity.kt
+++ app/src/main/java/com/vishal2376/rebooked/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.vishal2376.rebooked
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.vishal2376.rebooked.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding get() = _binding!!
+
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +25,33 @@ class MainActivity : AppCompatActivity() {
         //only light mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
 
+        // Set up the BottomNavigationView with the NavController
+        binding.bottomNav.setupWithNavController(navController)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.itemHome -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.itemSearch -> {
+                    navController.navigate(R.id.searchFragment)
+                    true
+                }
+                R.id.itemUpload -> {
+                    navController.navigate(R.id.uploadFragment)
+                    true
+                }
+                R.id.itemProfile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }

d9a9796a616a46c2e507ee62237eae9cb2f0214a
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-04 22:03:15 +0530

    update: dropdown implemented

--- app/src/main/java/com/vishal2376/rebooked/fragments/nav/UploadFragment.kt
+++ app/src/main/java/com/vishal2376/rebooked/fragments/nav/UploadFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.vishal2376.rebooked.R
 import com.vishal2376.rebooked.databinding.FragmentUploadBinding
 
 
@@ -19,6 +21,19 @@ class UploadFragment : Fragment() {
     ): View {
         _binding = FragmentUploadBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categoryAdapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            resources.getStringArray(R.array.book_categories)
+        )
+        binding.dropdown.setAdapter(categoryAdapter)
+
+
     }
 
     override fun onDestroyView() {
